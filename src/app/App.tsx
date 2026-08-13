e883e3918d714c386905d447da9ec85d8fa9d01b
Gene Elpie Landoy <g.landoyelpie@gmail.com>
2026-08-13 11:34:09 +0800

    Polish responsive mental health support site

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,14 @@
+import { useEffect } from 'react';
 import { HomePage } from '../presentation/pages/HomePage';
 
 export function App() {
+  useEffect(() => {
+    if (!window.location.hash) return;
+
+    window.requestAnimationFrame(() => {
+      document.querySelector(window.location.hash)?.scrollIntoView();
+    });
+  }, []);
+
   return <HomePage />;
 }
